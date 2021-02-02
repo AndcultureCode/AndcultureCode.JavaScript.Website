5b414610ce36cef6dc00d3f1ef1104f6ddd3b017
nhinhdao <ndao2803@gmail.com>
2021-02-02 13:10:00 -0500

    Added validation for phone number

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -10,6 +10,7 @@ const Input = class extends React.Component {
             fieldActive:      false,
             placeholderValue: this.props.name,
             error:            false,
+            isInvalidInput:   false,
         }
 
         this._updateInputValue = this._updateInputValue.bind(this);
@@ -21,23 +22,17 @@ const Input = class extends React.Component {
         this.setState({
             fieldActive:      true,
             placeholderValue: '',
+            error:            false,
+            isInvalidInput:   false,
         })
     }
 
     _disableField(e) {
-        if (this.props.type === "email") {
-            const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (!pattern.test(e.target.value)) {
-                this.setState({
-                    error: true,
-                })
-                return;
-            }
-        }
         if (e.target.value === "") {
             this.setState({
                 fieldActive:      false,
                 placeholderValue: this.props.name,
+                isInvalidInput:   false,
             })
 
             if (this.props.isRequired) {
@@ -46,9 +41,28 @@ const Input = class extends React.Component {
                 })
             }
         } else {
+            if (this.props.type === "email") {
+                const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!pattern.test(e.target.value)) {
+                    this.setState({
+                        isInvalidInput: true,
+                    })
+                    return;
+                }
+            }
+            if (this.props.name === "phone") {
+                const pattern = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/;
+                if (!pattern.test(e.target.value)) {
+                    this.setState({
+                        isInvalidInput: true,
+                    })
+                    return;
+                }
+            }
             if (this.props.isRequired) {
                 this.setState({
-                    error: false,
+                    error:          false,
+                    isInvalidInput: false,
                 })
             }
         }
@@ -96,6 +110,10 @@ const Input = class extends React.Component {
                 { // if
                     this.state.error &&
                     <span className = "a-label__error">please enter your { this.props.name }</span>
+                }
+                { // if
+                    this.state.isInvalidInput &&
+                    <span className = "a-label__error">please enter a valid { this.props.name }</span>
                 }
             </fieldset>
         )
