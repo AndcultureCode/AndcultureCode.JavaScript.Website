01eeea25cb2a54d7ccbe2c702c14a6d040bdd8c3
Nhinh <ndao2803@gmail.com>
2021-02-26 11:04:21 -0500

    Feature/landing page for 2021 florida conference (#150)
    * Setup landing page
    
    * Setup landing page and route
    
    * Updated styles
    
    * Updated styles
    
    * Setup landing page and routs
    
    * Updated styles
    
    * Refactorred
    
    * Refactorred
    
    * Refactorred
    
    * Added phone extension field to EventRegistrationForm
    
    * Added phone extension field to EventRegistrationForm
    
    * Refactored
    
    * Refactored
    
    * Refactored
    
    * Refactored phone and extension fields
    
    * Updated phone and extension styling
    
    * Refactored
    
    * Update langing page styling
    
    * Update langing page styling
    
    * Refactored
    
    * Refactored
    
    * Refactored
    
    * Default to empty string for blank inputs
    
    * Reformatted
    
    Co-authored-by: Jim Stevenson <jstevenson@andculture.com>

--- src/components/molecules/EventRegistrationForm.js
+++ src/components/molecules/EventRegistrationForm.js
@@ -39,27 +39,27 @@ const EventRegistrationForm = class extends React.Component {
             fields: [
                 {
                     name: "name",
-                    value: formData.name,
+                    value: formData.name ?? "",
                 },
                 {
                     name: "email",
-                    value: formData.email
+                    value: formData.email ?? "",
                 },
                 {
                     name: "jobtitle",
-                    value: formData.title
+                    value: formData.title ?? "",
                 },
                 {
                     name: "phone",
-                    value: formData.phone
+                    value: formData.phone ?? "",
                 },
                 {
                     name: "extension",
-                    value: formData.extension
+                    value: formData.extension ?? "",
                 },
                 {
                     name: "company",
-                    value: formData.companyName
+                    value: formData.companyName ?? "",
                 }
             ],
             context: pageContext,
@@ -106,21 +106,24 @@ const EventRegistrationForm = class extends React.Component {
     }
 
     render() {
-        const formClass   = "o-registration-form";
+        const formClass      = "o-registration-form";
+        const { formData }   = this.state;
+        const { lightTheme } = this.props;
 
         let buttonClass = "a-button";
-        buttonClass += this.props.lightTheme ? " -light " : "";
+        buttonClass += lightTheme ? " -light " : "";
         buttonClass += this.state.formIsValid ? "" : " -disabled";
 
-        const headerClass = this.props.lightTheme ? " -light " : "";
+        const headerClass = lightTheme ? " -light " : "";
+
 
         return (
             <form className = { formClass }
-                name={ this.props.formName }
-                method="POST"
-                data-netlify="true">
-                <div className = { `${formClass}__container` }>
-                    <div className = { `${formClass}__wrapper` }>
+                name         = { this.props.formName }
+                method       = "POST"
+                data-netlify = "true">
+                <div className = { `${ formClass }__container` }>
+                    <div className = { `${ formClass }__wrapper` }>
                         {
                             this.state.submitted &&
                             <React.Fragment>
@@ -133,61 +136,61 @@ const EventRegistrationForm = class extends React.Component {
                             <React.Fragment>
                                 <h2 className = { headerClass } aria-label="Registration form header">{ this.props.header }</h2>
                                 <p>{ this.props.subHeader }</p>
-                                <div className = { `${formClass}__input-container` }>
+                                <div className = { `${ formClass }__input-container` }>
                                     <div className= "-left">
                                         <Input
                                             id                 = "registration_form_name_input"
                                             inputValueCallback = { this._setInputValue }
                                             isRequired         = { true }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "name"
                                             type               = "text"
-                                            value              = { this.state.formData.name }/>
+                                            value              = { formData.name }/>
                                         <Input
                                             id                 = "registration_form_title_input"
                                             inputValueCallback = { this._setInputValue }
                                             isRequired         = { false }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "title"
                                             type               = "text"
-                                            value              = { this.state.formData.title }/>
+                                            value              = { formData.title }/>
                                         <Input
                                             id                 = "registration_form_company_name_input"
                                             inputValueCallback = { this._setInputValue }
                                             isRequired         = { false }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "companyName"
                                             placeholder        = "company name"
                                             type               = "text"
-                                            value              = { this.state.formData.companyName }/>
+                                            value              = { formData.companyName }/>
                                     </div>
                                     <div className = "-right">
                                         <Input
                                             id                 = "registration_form_email_input"
                                             inputValueCallback = { this._setInputValue }
                                             isRequired         = { true }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "email"
                                             type               = "email"
-                                            value              = { this.state.formData.email }/>
+                                            value              = { formData.email }/>
                                         <Input
                                             id                 = "registration_form_phone_number_input"
                                             inputValueCallback = { this._setInputValue }
                                             isRequired         = { false }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "phone"
                                             placeholder        = "phone number"
                                             type               = "text"
-                                            value              = { this.state.formData.phone }/>
+                                            value              = { formData.phone }/>
                                         <Input
                                             id                 = "registration_form_phone_extension_input"
                                             inputValueCallback = { this._setInputValue }
-                                            isDisabled         = { StringUtils.isEmpty(this.state.formData.phone) }
+                                            isDisabled         = { StringUtils.isEmpty(formData.phone) }
                                             isRequired         = { false }
-                                            lightTheme         = { this.props.lightTheme }
+                                            lightTheme         = { lightTheme }
                                             name               = "extension"
                                             type               = "text"
-                                            value              = { this.state.formData.extension }/>
+                                            value              = { formData.extension }/>
                                     </div>
                                 </div>
                                 <div className = "o-registration-form__buttons">
@@ -196,7 +199,7 @@ const EventRegistrationForm = class extends React.Component {
                                         onClick   = { this._onSubmitClick }
                                         className = { buttonClass }>
                                         Submit
-                                    </button>
+                                    </button>gi
                                 </div>
                             </React.Fragment>
                         }
